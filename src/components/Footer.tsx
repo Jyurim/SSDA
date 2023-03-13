088850974e6b190bf3ae6a5f6933f837107ab1df
HanDaWoon <bookpia.han@gmail.com>
2023-03-14 01:03:07 +0900

    Feat : Navbar,Footer 생성 및 적용, 로고 추가

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+const Footer = () => {
+  return (
+    <footer className="rounded-lg p-4  md:flex md:items-center md:justify-between md:p-6">
+      <span className="text-sm text-gray-500 dark:text-gray-400 sm:text-center">
+        © 2023{" "}
+        <a href="https://flowbite.com/" className="hover:underline">
+          Flowbite™
+        </a>
+        . All Rights Reserved.
+      </span>
+      <ul className="mt-3 flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6 ">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Privacy Policy
+          </a>
+        </li>
+        <li>
+          <a href="#" className="mr-4 hover:underline md:mr-6">
+            Licensing
+          </a>
+        </li>
+        <li>
+          <a href="#" className="hover:underline">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import Link from "next/link";
+
+const Navbar = () => {
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  return (
+    <nav className="bg-gray-100">
+      <div className="mx-auto  px-4">
+        <div className="flex justify-between">
+          {/* 메뉴 */}
+          <div className="flex space-x-4">
+            <div>
+              <Link href="/" className="flex items-center py-5 px-2 text-gray-700">
+                <Image src={logo} alt="" width={80} />
+                <span className="pl-4 font-bold">Home</span>
+              </Link>
+            </div>
+            <div className="hidden items-center space-x-1 md:flex">
+              <Link href="features" className="py-5 px-3 text-gray-700 hover:text-gray-900">
+                Features
+              </Link>
+            </div>
+          </div>
+
+          {/* 메뉴2 */}
+          <div className="hidden items-center space-x-1 md:flex">
+            <Link
+              href="#"
+              className="text-white-900 rounded bg-blue-400 py-2 px-3 transition duration-300 hover:bg-yellow-300 hover:text-yellow-800"
+            >
+              Login
+            </Link>
+            <Link
+              href="#"
+              className="rounded bg-yellow-400 py-2 px-3 text-yellow-900 transition duration-300 hover:bg-yellow-300 hover:text-yellow-800"
+            >
+              Signup
+            </Link>
+          </div>
+
+          {/* mobile menu */}
+          <div className="flex items-center md:hidden">
+            <button onClick={() => setMenuToggle(!menuToggle)}>
+              {menuToggle ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      {/* mobile menu items */}
+      {/* <div className={classNames("md:hidden", { hidden: !menuToggle })}> */}
+      <div className={!menuToggle ? "md:hidden" : ""}>
+        <Link href="#" className="block py-2 px-4 text-sm hover:bg-gray-200">
+          Features
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
