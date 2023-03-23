b50feb521c6cfde02bd47db68cc17bf8a59d299c
HanDaWoon <bookpia.han@gmail.com>
2023-03-23 17:58:31 +0900

    Fix : Warning index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <section className="bg-[url('../../img/landing_bg.jpg')]">
+      <section className="bg-[url('../../public/landing_bg.jpg')]">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
           <div className="mr-auto place-self-center lg:col-span-7">
             <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
@@ -23,7 +23,7 @@ const Home = () => {
               you again.
             </p>
 
-            <button className="inline-flex items-center justify-center rounded bg-blue-600 px-5 py-3 py-2 px-4 text-center text-base font-medium font-bold text-white hover:bg-blue-700">
+            <button className="inline-flex items-center justify-center rounded bg-blue-600 px-5 py-3 text-center text-base font-bold text-white hover:bg-blue-700">
               Try For Free !
             </button>
           </div>
