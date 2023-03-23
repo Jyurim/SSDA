d5917aa55188ddd28ed5f85f2c73b32eae60fd5c
HanDaWoon <bookpia.han@gmail.com>
2023-03-23 16:34:56 +0900

    Fix : Merge conflict and Add <Head>....</Head>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,37 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 const Home = () => {
-return(
-<section className="bg-[url('../../img/landing_bg.jpg')]">
-  <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
-      <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100">Write Your Own.</h1>
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-gray-100">Make Only One.</h1>
-          <p className="max-w-2xl mb-6 font-light text-gray-200 lg:mb-8 md:text-lg lg:text-xl ">Its been a long day without you my friend. and i will tell you all about it when i see you again.</p>
-             
-          <button className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Try For Free !
-          </button>
+  return (
+    <>
+      <Head>
+        <title>SSDA</title>
+        <meta name="description" content="Make your own" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section className="bg-[url('../../img/landing_bg.jpg')]">
+        <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+          <div className="mr-auto place-self-center lg:col-span-7">
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
+              Write Your Own.
+            </h1>
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
+              Make Only One.
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-gray-200 md:text-lg lg:mb-8 lg:text-xl ">
+              Its been a long day without you my friend. and i will tell you all about it when i see
+              you again.
+            </p>
 
-          
-      </div>
-      <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          
-      </div>                
-  </div>
-</section>)};
- 
+            <button className="inline-flex items-center justify-center rounded bg-blue-600 px-5 py-3 py-2 px-4 text-center text-base font-medium font-bold text-white hover:bg-blue-700">
+              Try For Free !
+            </button>
+          </div>
+          <div className="hidden lg:col-span-5 lg:mt-0 lg:flex"></div>
+        </div>
+      </section>
+    </>
+  );
+};
 
 export default Home;
