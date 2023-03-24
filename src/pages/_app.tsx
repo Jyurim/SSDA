2dbf025dd867f2b2352b09a216ecbee7891f443f
HanDaWoon <bookpia.han@gmail.com>
2023-03-24 12:48:08 +0900

    Fix : fortawesome with ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import type { AppProps } from "next/app";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
