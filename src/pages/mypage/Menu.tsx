0ce7d855e6861c6be9bb3220cc4c00063be53a40
ur <dorosi150@gmail.com>
2023-03-27 19:34:08 +0900

    Fix: Invalid DOM property
    favorites.tsx
    stroke-width   ->   strokeWidth
    stroke-linecap  ->  strokeLinecap
    stroke-linejoin  ->  storkeLinejoin
    Menu.tsx
    file-rule   ->   fileRule
    clip-rule   ->   clipRule

--- src/pages/mypage/Menu.tsx
+++ src/pages/mypage/Menu.tsx
@@ -41,9 +41,9 @@ const Menu = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="ml-3 flex-1 whitespace-nowrap">Users</span>
@@ -85,9 +85,9 @@ const Menu = () => {
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
-                    fill-rule="evenodd"
+                    fillRule="evenodd"
                     d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
-                    clip-rule="evenodd"
+                    clipRule="evenodd"
                   ></path>
                 </svg>
                 <span className="ml-3 flex-1 whitespace-nowrap">회원탈퇴</span>
