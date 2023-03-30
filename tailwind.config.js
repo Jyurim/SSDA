df86f8fc17e292b0dc198841ee41908df1bc728a
HanDaWoon <bookpia.han@gmail.com>
2023-03-30 17:37:59 +0900

    Feat : Install flowbite
    "flowbite": "^1.6.4"
    "flowbite-react": "^0.4.2"
    Config tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,54 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite-react/**/*.js",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      'white': '#ffffff',
-      'black': '#000000',
-      'gray': {
-        '50': '#f9fafb',
-        '100': '#f4f5f7',
-        '200': '#e5e7eb',
-        '300': '#d2d6dc',
-        '400': '#9fa6b2',
-        '500': '#6b7280',
-        '600': '#4b5563',
-      },
-      'red': {
-        '50': '#fdf2f2',
-        '100': '#fde8e8',
-        '200': '#fbd5d5',
-        '300': '#f8b4b4',
-        '400': '#f98080',
-        '500': '#f05252',
-        '600': '#e02424',
-        '700': '#c81e1e',
-        '800': '#9b1c1c',
-        '900': '#771d1d',
-      },
-      'blue': {
-        '50': '#eff6ff',
-        '100': '#dbeafe',
-        '200': '#bfdbfe',
-        '300': '#93c5fd',
-        '400': '#60a5fa',
-        '500': '#3b82f6',
-        '600': '#2563eb',
-        '700': '#1d4ed8',
-        '800': '#1e40af',
-        '900': '#1e3a8a',
-      },
-    },
-    extend: {},
-  },
-  plugins: [],
-}
+  plugins: [require("flowbite/plugin")],
+};
