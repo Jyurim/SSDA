491fd987c923de224d88292fb5887447658bff38
HanDaWoon <bookpia.han@gmail.com>
2023-05-05 10:07:13 +0900

    SSDA-23 Fix: Canvas 크기 조정

--- src/pages/demo.tsx
+++ src/pages/demo.tsx
@@ -5,28 +5,29 @@ import { Button } from "flowbite-react";
 
 const Canvas = () => {
   const canvasRef = useRef<CanvasDraw>(null);
+  console.log(canvasRef);
   return (
-    <section className="flex items-center justify-center">
+    <section className="flex h-screen items-center justify-center">
       <CanvasDraw
+        style={{ minHeight: 911, minWidth: 1608 }}
+        canvasHeight={911}
+        canvasWidth={1608}
         ref={canvasRef}
-        canvasWidth={550}
         lazyRadius={0}
         brushRadius={2}
         brushColor="black"
         hideGrid={true}
         imgSrc={Testimg.src}
+        enablePanAndZoom={true}
       />
-      <div>
-        {
-          <>
-            <Button onClick={() => canvasRef?.current?.undo()}>Undo</Button>
-            <Button onClick={() => canvasRef?.current?.clear()}>Clear</Button>
-            <Button onClick={() => canvasRef?.current?.getDataURL("png", false, "")}>
-              getDataURL
-            </Button>
-          </>
-        }
+      <div className="">
+        <Button onClick={() => canvasRef?.current?.undo()}>Undo</Button>
+        <Button onClick={() => canvasRef?.current?.clear()}>Clear</Button>
+        <Button onClick={() => console.log(canvasRef?.current?.getDataURL("png", true, ""))}>
+          getDataURL
+        </Button>
       </div>
+      <div></div>
     </section>
   );
 };
