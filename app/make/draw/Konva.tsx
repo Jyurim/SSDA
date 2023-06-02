6432da61453882278009147a77aaeb1c804a341c
HanDaWoon <bookpia.han@gmail.com>
2023-06-02 16:21:14 +0900

    Fix : add allow cors header

--- app/make/draw/Konva.tsx
+++ app/make/draw/Konva.tsx
@@ -170,6 +170,9 @@ const Konva: FunctionComponent = () => {
       headers: {
         Authorization: `Bearer ${session?.user?.token}`,
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "*",
       },
       body: JSON.stringify({
         imageBase64,
