0093c89743d738b9d781c54b430cee4ea0fc0361
HanDaWoon <bookpia.han@gmail.com>
2023-06-14 20:26:31 +0900

    SSDA-46 Feat : favicon 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
 import Providers from "@components/Providers";
 import Loading from "./info/loading";
+import Head from "next/head";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,6 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="shortcut icon" href="./page.tsxfavicon.ico" type="image/x-icon" />
+      </Head>
       <body className={inter.className}>
         <Providers>
           <Navbar />
