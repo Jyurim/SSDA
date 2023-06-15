e1a65faa76eb9c01782c017132f0046ccc767b07
Choijake <shameless8@naver.com>
2023-06-15 17:44:05 +0900

    Feat:히어로 문구 변경

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,160 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <section className="min-h-3/4">
-      <div className="bg-[url('../public/landing_bg.jpg')]">
-        <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
-          <div className="mr-auto place-self-center lg:col-span-7">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
-              Write Your Own.
-            </h1>
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
-              Make Only One.
-            </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-200 md:text-lg lg:mb-8 lg:text-xl ">
-              Its been a long day without you my friend. and i will tell you all about it when i see
-              you again.
-            </p>
-
-            <button className="inline-flex items-center justify-center rounded bg-blue-600 px-5 py-3 text-center text-base font-bold text-white hover:bg-blue-700">
-              Try For Free !
-            </button>
+      <div>
+        <section className="min-h-3/4">
+          <div className="bg-[url('../public/landing_bg.jpg')]">
+            <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0">
+              <div className="mr-auto place-self-center lg:col-span-7">
+                <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
+                  내 손에서 태어난 글씨
+                </h1>
+                <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight text-gray-100 md:text-5xl xl:text-6xl">
+                  단 하나, 그대만의 글씨로.
+                </h1>
+                <p className="mb-6 max-w-2xl font-light text-gray-200 md:text-lg lg:mb-8 lg:text-xl">
+                  Its been a long day without you my friend. and i will tell you all about it when i see you again.
+                </p>
+                <button className="inline-flex items-center justify-center rounded bg-blue-600 px-5 py-3 text-center text-base font-bold text-white hover:bg-blue-700">
+                  Try For Free!
+                </button>
+              </div>
+              <div className="hidden lg:col-span-5 lg:mt-0 lg:flex"></div>
+            </div>
           </div>
-          <div className="hidden lg:col-span-5 lg:mt-0 lg:flex"></div>
-        </div>
+        </section>
+
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto flex flex-wrap">
+            <div className="flex flex-wrap w-full">
+              <div className="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
+                <div className="flex relative pb-12">
+                  <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                    >
+                      <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow pl-4">
+                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 1</h2>
+                    <p className="leading-relaxed">
+                      회원가입 버튼을 누르고 우리 서비스의 회원이 되세요!
+                    </p>
+                  </div>
+                </div>
+                <div className="flex relative pb-12">
+                  <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                    >
+                      <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow pl-4">
+                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 2</h2>
+                    <p className="leading-relaxed">
+                      사이트 상단의 메뉴에 있는 만들기 버튼을 누르세요
+                    </p>
+                  </div>
+                </div>
+                <div className="flex relative pb-12">
+                  <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                    >
+                      <circle cx="12" cy="5" r="3"></circle>
+                      <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow pl-4">
+                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 3</h2>
+                    <p className="leading-relaxed">
+                      그리기 버튼을 누르면 4X8 격자가 보입니다</p>
+                  </div>
+                </div>
+                <div className="flex relative pb-12">
+                  <div className="h-full w-10 absolute inset-0 flex items-center justify-center">
+                    <div className="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                  </div>
+                  <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                    >
+                      <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                      <circle cx="12" cy="7" r="4"></circle>
+                    </svg>
+                  </div>
+                  <div className="flex-grow pl-4">
+                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">STEP 4</h2>
+                    <p className="leading-relaxed">
+                      격자에 있는 28개의 글자에 해당하는 손글씨를 입력하면 나만의 손글씨가 생성됩니다!
+                    </p>
+                  </div>
+                </div>
+                <div className="flex relative">
+                  <div className="flex-shrink-0 w-10 h-10 rounded-full bg-yellow-500 inline-flex items-center justify-center text-white relative z-10">
+                    <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-5 h-5"
+                        viewBox="0 0 24 24"
+                    >
+                      <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                      <path d="M22 4L12 14.01l-3-3"></path>
+                    </svg>
+                  </div>
+                  <div className="flex-grow pl-4">
+                    <h2 className="font-medium title-font text-sm text-gray-900 mb-1 tracking-wider">FINISH</h2>
+                    <p className="leading-relaxed">나만의 폰트를 게시판에 자유롭게 공유해보세요</p>
+                  </div>
+                </div>
+              </div>
+              <img
+                  className="lg:w-1/2 md:w-1/2 object-cover object-center rounded-lg md:mt-0 mt-12"
+                  src="/img.png"
+                  alt="step"
+              />
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
   );
 }
